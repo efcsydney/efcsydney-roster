faaf7c157d0fbf202cfa2beae685b63d205ba209
josephj <josephj6802@gmail.com>
2017-11-04 09:47:56 +1100

    Add basic QuarterView

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,37 @@
-import React, { Component } from "react";
-import SelectedFoods from "./SelectedFoods";
-import FoodSearch from "./FoodSearch";
+import React, { Component } from 'react';
+import SelectedFoods from './SelectedFoods';
+import FoodSearch from './FoodSearch';
+import QuarterView from './components/QuarterView';
+import styled from 'styled-components';
+import data from './data.json';
 
-class App extends Component {
+export default class App extends Component {
   state = {
     selectedFoods: []
   };
-
   removeFoodItem = itemIndex => {
     const filteredFoods = this.state.selectedFoods.filter(
       (item, idx) => itemIndex !== idx
     );
     this.setState({ selectedFoods: filteredFoods });
   };
-
   addFood = food => {
     const newFoods = this.state.selectedFoods.concat(food);
     this.setState({ selectedFoods: newFoods });
   };
-
   render() {
     const { selectedFoods } = this.state;
-
     return (
-      <div className="App">
-        <div className="ui text container">
-          <SelectedFoods
-            foods={selectedFoods}
-            onFoodClick={this.removeFoodItem}
-          />
-          <FoodSearch onFoodClick={this.addFood} />
-        </div>
-      </div>
+      <Wrapper>
+        <QuarterView date={new Date()} data={data} />
+        <SelectedFoods
+          foods={selectedFoods}
+          onFoodClick={this.removeFoodItem}
+        />
+        <FoodSearch onFoodClick={this.addFood} />
+      </Wrapper>
     );
   }
 }
 
-export default App;
+const Wrapper = styled.div`margin: 10px;`;
