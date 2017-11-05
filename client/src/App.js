f2101e64a96a9121d1a66dd9490313d072154ee2
James <james1239090@gmail.com>
2017-11-05 13:43:51 +1100

    Arrow buttons basic

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import FoodSearch from './FoodSearch';
 import QuarterView from './components/QuarterView';
 import styled from 'styled-components';
 import data from './data.json';
+import leftArrowIcon from './assets/arrow_left.svg';
+import rightArrowIcon from './assets/arrow_right.svg';
 
 export default class App extends Component {
   state = {
@@ -23,7 +25,15 @@ export default class App extends Component {
     const { selectedFoods } = this.state;
     return (
       <Wrapper>
-        <QuarterView date={new Date()} data={data} />
+        <ViewWrapper>
+          <QuarterView date={new Date()} data={data} />
+          <PrevButton>
+            <img src={leftArrowIcon} />
+          </PrevButton>
+          <NextButton>
+            <img src={rightArrowIcon} />
+          </NextButton>
+        </ViewWrapper>
         <SelectedFoods
           foods={selectedFoods}
           onFoodClick={this.removeFoodItem}
@@ -35,3 +45,23 @@ export default class App extends Component {
 }
 
 const Wrapper = styled.div`margin: 10px;`;
+const ViewWrapper = styled.div`position: relative;`;
+const Arrow = styled.button`
+  position: absolute;
+  top: 50%;
+  border-radius: 50%;
+  border-width: 0;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  line-height: 0;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.5);
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  img {
+    margin: 0 auto;
+    display: inline-block;
+  }
+`;
+const PrevButton = styled(Arrow)`left: 10px;`;
+const NextButton = styled(Arrow)`right: 10px;`;
