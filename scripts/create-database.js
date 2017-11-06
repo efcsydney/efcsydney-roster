4cabfda62d67a7ccadf17400eb947fad783e8f0d
Jimmy Wong <imagegospel@gmail.com>
2017-11-06 23:57:18 +1100

    Add basic db adaptor

---
+++ scripts/create-database.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const env       = process.env.NODE_ENV || 'development';
+const config    = require(__dirname + '/../config/database.json')[env];
+
+const sequelize = new Sequelize(undefined, config.username, null, {
+  dialect: config.dialect,
+  host: config.host
+});
+
+return sequelize.query(`CREATE DATABASE ${config.database}`)
+.catch((err) => console.log(err))
+.lastly(() => sequelize.close());
