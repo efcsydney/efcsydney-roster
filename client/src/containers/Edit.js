6f8f0b5493200c95bbc8e0fdd203264b544fe56a
James <james1239090@gmail.com>
2017-11-12 13:03:23 +1100

    Add Combo Dropdown Input

--- client/src/containers/Edit.js
+++ client/src/containers/Edit.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import Modal from '../components/Modal';
 import styled from 'styled-components';
+import { Creatable } from 'react-select';
+import 'react-select/dist/react-select.css';
+import _ from 'lodash';
+
+function getOptions(names) {
+  names = names.map(name => ({ value: name, label: name }));
+  names.unshift({ value: '', label: '-' });
+
+  return names;
+}
 
 export default class Edit extends Component {
   static propTypes = {
@@ -12,26 +22,28 @@ export default class Edit extends Component {
     onSave: PropTypes.func,
     role: PropTypes.string
   };
-  handleNameChange = e => {
-    const value = e.target.value;
-
+  handleNameChange = value => {
     this.setState({ selectedName: value });
+
   };
   handleSaveClick = form => {
     const { onSave } = this.props;
-
+    if (_.isPlainObject(form.name)) {
+      form.name = form.name.label;
+    }
     onSave(form);
   };
   constructor(props) {
     super(props);
 
     this.state = {
+      names: props.names,
       selectedName: props.member
     };
   }
   render() {
-    const { date, member, names, onSave, role, ...otherProps } = this.props; // eslint-disable-line
-    const { selectedName } = this.state;
+    const { date, member, onSave, role, ...otherProps } = this.props; // eslint-disable-line
+    const { selectedName, names } = this.state;
     const formattedDate = moment(date).format('DD MMM, YYYY');
 
     return (
@@ -48,10 +60,12 @@ export default class Edit extends Component {
           <Row>
             <Label>Name</Label>
             <span>
-              <Select value={selectedName} onChange={this.handleNameChange}>
-                <option value="">-</option>
-                {names.map((name, i) => <option key={i}>{name}</option>)}
-              </Select>
+              <Select
+                multi={false}
+                value={selectedName}
+                onChange={this.handleNameChange}
+                options={getOptions(names)}
+              />
             </span>
           </Row>
           <Row>
@@ -91,15 +105,7 @@ const Label = styled.label`
   text-align: right;
   width: 80px;
 `;
-const Select = styled.select`
-  background-color: #fff;
-  border-radius: 3px;
-  border: 1px solid #ccc;
-  box-shadow: inset 0 2px 1px 0 rgba(185, 185, 185, 0.3);
-  display: inline-block;
-  outline: none;
-  padding: 10px;
-`;
+const Select = styled(Creatable)`width: 180px;`;
 const Button = styled.button.attrs({ type: 'button' })`
   background-color: #588c25;
   border-radius: 3px;
