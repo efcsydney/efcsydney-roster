a7058b6362d566740ff48b8bb552eb6aa19e82af
LiamLai <liam.lai@tabcorp.com.au>
2017-11-16 09:07:46 +1100

    deploy to heroku for test

--- deploy/heroku.sh
+++ deploy/heroku.sh
@@ -1,4 +1,8 @@
+#/bin/sh
 current_branch=`git branch | grep \* | cut -d ' ' -f2`
+
+#Check install heroku 
+which herokua || brew install heroku 
 
 git add .
 git commit -m "deploy to heroku for test"
