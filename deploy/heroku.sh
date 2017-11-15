3363cd94cd7f650bd1e738868175a2aea1be8ceb
LiamLai <liam.lai@tabcorp.com.au>
2017-11-16 09:02:22 +1100

    deploy to heroku for test

--- deploy/heroku.sh
+++ deploy/heroku.sh
@@ -1,4 +1,6 @@
+current_branch=`git branch | grep \* | cut -d ' ' -f2`
+
 git add .
 git commit -m "deploy to heroku for test"
 heroku git:remote -a efcsydney-roster
-git push -f heroku dploy:master
+git push -f heroku $current_branch:master
