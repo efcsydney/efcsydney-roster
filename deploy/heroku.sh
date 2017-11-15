175b55708a4f5b0b1afacd69fb127d411f62c4d0
LiamLai <liam.lai@tabcorp.com.au>
2017-11-16 09:14:38 +1100

    deploy to heroku for test

--- deploy/heroku.sh
+++ deploy/heroku.sh
@@ -1,8 +1,11 @@
 #/bin/sh
 current_branch=`git branch | grep \* | cut -d ' ' -f2`
 
-#Check install heroku 
-which heroku || brew install heroku 
+#Check install heroku  
+machine_type=`uname -s`
+if [ $machine_type = "Darwin" ]; then
+    which herokua || brew install heroku 
+fi
 
 #Deploy Everything to heroku master branch 
 git add .
@@ -10,4 +13,7 @@ git commit -m "deploy to heroku for test"
 heroku git:remote -a efcsydney-roster
 git push -f heroku $current_branch:master
 
-echo "Check your latest code on https://efcsydney-roster.herokuapp.com/"
+Yellow='\033[0;33m'
+White='\033[0;0m'
+echo 
+echo "Check your latest code on$Yellow https://efcsydney-roster.herokuapp.com/ $White"
