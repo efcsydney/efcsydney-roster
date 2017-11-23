48bf0f9a76241b67468f8d26a176fcea61bb4297
Jimmy Wong <jimmy.wong@invoice2go.com>
2017-11-23 20:03:26 +1100

    Remove production lock and change default to use DB

--- api/service/factory.js
+++ api/service/factory.js
@@ -4,26 +4,26 @@ const EventMapper = require('../mapper/event-mapper').EventMapper;
 const MockEventMapper = require('../mapper/mock-event-mapper').MockEventMapper;
 const EventService = require('./events-service').EventService;
 const MockEventService = require('./mock-events-service').MockEventService;
-class Factory{
-  static getEventRepository(req){
-    if(process.env.NODE_ENV === 'production' || req.query.mock === "false"){
-      return EventRepository;
-    } else {
+class Factory {
+  static getEventRepository(req) {
+    if (req.query.mock === 'true') {
       return MockRepository;
+    } else {
+      return EventRepository;
     }
   }
-  static getDataMapper(req){
-    if(process.env.NODE_ENV === 'production' || req.query.mock === "false"){
-      return EventMapper;
-    } else {
+  static getDataMapper(req) {
+    if (req.query.mock === 'true') {
       return MockEventMapper;
+    } else {
+      return EventMapper;
     }
   }
   static getEventService(req) {
-    if(process.env.NODE_ENV === 'production' || req.query.mock === "false"){
-      return EventService;
-    } else {
+    if (req.query.mock === 'true') {
       return MockEventService;
+    } else {
+      return EventService;
     }
   }
 }
