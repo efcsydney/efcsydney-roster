c5e8d4ce1829d8ca14820a703edc185b993ebfdc
josephj <josephj6802@gmail.com>
2017-11-25 22:49:46 +1100

    Add more sample data for demo

--- db/seed/20171111110130-initialize-calendars.js
+++ db/seed/20171111110130-initialize-calendars.js
@@ -47,6 +47,91 @@ module.exports = {
       date: '2017-12-10',
       createdAt: new Date(),
       updatedAt: new Date()
+    }, {
+      id: 10,
+      date: '2018-01-21',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 11,
+      date: '2018-02-04',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 12,
+      date: '2018-02-18',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 13,
+      date: '2018-03-04',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 14,
+      date: '2018-03-18',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 15,
+      date: '2017-07-02',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 16,
+      date: '2017-07-09',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 17,
+      date: '2017-07-16',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 18,
+      date: '2017-07-23',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 19,
+      date: '2017-07-30',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 20,
+      date: '2017-08-06',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 21,
+      date: '2017-08-13',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 22,
+      date: '2017-08-20',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 23,
+      date: '2017-08-27',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 24,
+      date: '2017-09-10',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 25,
+      date: '2017-09-17',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 26,
+      date: '2017-09-24',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
   },
 
--- db/seed/20171111110851-initialize-events.js
+++ db/seed/20171111110851-initialize-events.js
@@ -442,6 +442,636 @@ module.exports = {
       positionId: 8,
       createdAt: new Date(),
       updatedAt: new Date()
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 10,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Gary Tang',
+      calendarDateId: 10,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 11,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 12,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 13,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Gary Tan',
+      calendarDateId: 13,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 14,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-07-02
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 15,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Bobby Lu',
+      calendarDateId: 15,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu',
+      calendarDateId: 15,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen',
+      calendarDateId: 15,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 15,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 15,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 15,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christian Yang',
+      calendarDateId: 15,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-07-09
+    },{
+      volunteerName: 'Rev. Jason Cheng',
+      calendarDateId: 16,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Dan Kao',
+      calendarDateId: 16,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Jenny Hsu',
+      calendarDateId: 16,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen & Betty Chen',
+      calendarDateId: 16,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 16,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 16,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 16,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christian Yang',
+      calendarDateId: 16,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-07-16
+    },{
+      volunteerName: 'Leo Chen',
+      calendarDateId: 17,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 17,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen',
+      calendarDateId: 17,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu',
+      calendarDateId: 17,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 17,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 17,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Robin Zhang',
+      calendarDateId: 17,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Joseph Chiang',
+      calendarDateId: 17,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-07-23
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 18,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Gary Tan',
+      calendarDateId: 18,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu',
+      calendarDateId: 18,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 18,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 18,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 18,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 18,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Joseph Chiang',
+      calendarDateId: 18,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-07-30
+    },{
+      volunteerName: 'Aaron Goh',
+      calendarDateId: 19,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Jennifer Chu',
+      calendarDateId: 19,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 19,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen',
+      calendarDateId: 19,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 19,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 19,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 19,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 19,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-08-06
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 20,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Bobby Lu',
+      calendarDateId: 20,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen',
+      calendarDateId: 20,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu & Joseph Chiang',
+      calendarDateId: 20,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 20,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 20,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 20,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 20,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-08-13
+    },{
+      volunteerName: 'Rev. Francis Chan',
+      calendarDateId: 21,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Dan Kao',
+      calendarDateId: 21,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Dan Kao',
+      calendarDateId: 21,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu',
+      calendarDateId: 21,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 21,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 21,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Robin Zhang',
+      calendarDateId: 21,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 21,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-08-20
+    },{
+      volunteerName: 'May Chien',
+      calendarDateId: 22,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 22,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu',
+      calendarDateId: 22,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen',
+      calendarDateId: 22,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 22,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 22,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 22,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Cheer Lin',
+      calendarDateId: 22,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-08-27
+    },{
+      volunteerName: 'Leo Chen',
+      calendarDateId: 23,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Jennifer Chu',
+      calendarDateId: 23,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Jenny Hsu',
+      calendarDateId: 23,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 23,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 23,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Raymond Tseng',
+      calendarDateId: 23,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 23,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 23,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-09-10
+    },{
+      volunteerName: 'Rev. Kian Holik',
+      calendarDateId: 24,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 24,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Dan Kao',
+      calendarDateId: 24,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Yvonne Lu',
+      calendarDateId: 24,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 24,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Robin Zhang',
+      calendarDateId: 24,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Robin Zhang',
+      calendarDateId: 24,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 24,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-09-17
+    },{
+      volunteerName: 'Kathleen Kao',
+      calendarDateId: 25,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Gary Tan',
+      calendarDateId: 25,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 25,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen',
+      calendarDateId: 25,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Betty Chen',
+      calendarDateId: 25,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Joseph Chiang',
+      calendarDateId: 25,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 25,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Christine Yang',
+      calendarDateId: 25,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    // 2017-09-24
+    },{
+      volunteerName: 'David Luis',
+      calendarDateId: 26,
+      positionId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Dan Kao',
+      calendarDateId: 26,
+      positionId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Jenny Hsu',
+      calendarDateId: 26,
+      positionId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Amy Chen & Betty Chen',
+      calendarDateId: 26,
+      positionId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Kai Chang',
+      calendarDateId: 26,
+      positionId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Joseph Chiang',
+      calendarDateId: 26,
+      positionId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Angela Sun',
+      calendarDateId: 26,
+      positionId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      volunteerName: 'Jenny Shao',
+      calendarDateId: 26,
+      positionId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
   ], {});
   },
