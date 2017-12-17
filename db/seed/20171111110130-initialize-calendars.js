4ffbf81d7f6eba5d770c583e12c784333cf7b287
Jimmy Wong <imagegospel@gmail.com>
2017-12-17 21:34:28 +1100

    Redesign seed utility (#62)
    * Redesign seed utility
    
    * Minor cleanup

--- db/seed/20171111110130-initialize-calendars.js
+++ db/seed/20171111110130-initialize-calendars.js
@@ -1,39 +1,20 @@
-'use strict';
+const getDateString = require('../../api/utilities/datetime-util')
+  .getDateString;
 
 module.exports = {
+  // This will populate roughly 10 years of data from 2017
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      'calendar_dates',
-      [
-        { id: 1, date: '2017-10-08' },
-        { id: 2, date: '2017-10-15' },
-        { id: 3, date: '2017-10-29' },
-        { id: 4, date: '2017-11-05' },
-        { id: 5, date: '2017-11-12' },
-        { id: 6, date: '2017-11-19' },
-        { id: 7, date: '2017-11-26' },
-        { id: 8, date: '2017-12-03' },
-        { id: 9, date: '2017-12-10' },
-        { id: 10, date: '2018-01-21' },
-        { id: 11, date: '2018-02-04' },
-        { id: 12, date: '2018-02-18' },
-        { id: 13, date: '2018-03-04' },
-        { id: 14, date: '2018-03-18' },
-        { id: 15, date: '2017-07-02' },
-        { id: 16, date: '2017-07-09' },
-        { id: 17, date: '2017-07-16' },
-        { id: 18, date: '2017-07-23' },
-        { id: 19, date: '2017-07-30' },
-        { id: 20, date: '2017-08-06' },
-        { id: 21, date: '2017-08-13' },
-        { id: 22, date: '2017-08-20' },
-        { id: 23, date: '2017-08-27' },
-        { id: 24, date: '2017-09-10' },
-        { id: 25, date: '2017-09-17' },
-        { id: 26, date: '2017-09-24' }
-      ],
-      {}
-    );
+    // First Sunday in 2017
+    const dateTime = new Date(2017, 0, 1);
+    const calendarDates = [];
+    for (i = 0; i < 500; i++) {
+      // Get date string and add to calendarDates
+      dateString = getDateString(dateTime);
+      calendarDates.push({ date: dateString });
+      // Jump to next Sunday
+      dateTime.setDate(dateTime.getDate() + 7);
+    }
+    return queryInterface.bulkInsert('calendar_dates', calendarDates);
   },
 
   down: (queryInterface, Sequelize) => {
