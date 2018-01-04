48bfa54ab0c6d729086ff67fc9291074fd75e538
Kyle Huang <yhua045@gmail.com>
2018-01-04 22:07:27 +1100

    Api mandarin service support (#75)
    * Added migration and seed files to create Service table and created necessary associations. Created seed file to generate Chinese services
    
    * Update unit/integration test.

--- api/data/event-repository.js
+++ api/data/event-repository.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize');
 const Event = require('../models/event').Event;
 const CalendarDate = require('../models/calendar-date').CalendarDate;
 const Position = require('../models/position').Position;
-
+const Service = require('../models/service').Service;
 const Op = Sequelize.Op;
 
 class EventRepository {
-  static getEventsByDateRange(dateRange) {
+  static getEventsByDateRange(dateRange, service) {
     const { from, to } = dateRange;
     return Event.findAll({
       include: [
@@ -15,7 +15,19 @@ class EventRepository {
           as: 'calendarDate',
           where: { date: { [Op.gte]: from, [Op.lte]: to } }
         },
-        { model: Position, as: 'position' }
+        {
+          model: Position,
+          as: 'position',
+          required: true,
+          include: [
+            {
+              model: Service,
+              as: 'service',
+              required: true,
+              where: { name: { [Op.eq]: service } }
+            }
+          ]
+        }
       ],
       order: [
         [{ model: CalendarDate, as: 'calendarDate' }, 'date', 'DESC'],
@@ -42,42 +54,6 @@ class EventRepository {
       ]
     });
   }
-
-  // Event, CalendarDate and Position should all be prepopulated
-  // static findOrCreateCalendarDate(date) {
-  //   return CalendarDate.findOrCreate({
-  //     where: { date: date },
-  //     defaults: { date: date }
-  //   });
-  // }
-
-  // static findOrCreatePosition(position) {
-  //   return Position.findOrCreate({
-  //     where: { name: position }
-  //   });
-  // }
-
-  // static addEvent(event) {
-  //   const calendarDatePromise = EventRepository.findOrCreateCalendarDate(
-  //     event.calendarDate.date
-  //   );
-  //   const positionPromise = EventRepository.findOrCreatePosition(
-  //     event.position.name
-  //   );
-
-  //   return Promise.all([calendarDatePromise, positionPromise]).then(function(
-  //     results
-  //   ) {
-  //     const [calendarDateResult, positionResult] = results;
-  //     const calendarDate = calendarDateResult[0];
-  //     const position = positionResult[0];
-  //     return Event.create({
-  //       volunteerName: event.volunteerName,
-  //       calendarDateId: calendarDate.id,
-  //       positionId: position.id
-  //     });
-  //   });
-  // }
 
   static updateEvent(event) {
     return Event.update(
---
+++ db/migrate/20171227012424-create-service-table.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'services',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        name: {
+          type: Sequelize.STRING,
+          unique: true
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('NOW')
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('NOW')
+        }
+      },
+      {
+        charset: 'utf8'
+      }
+    );
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('services');
+  }
+};
