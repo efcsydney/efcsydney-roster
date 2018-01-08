95ef8758532798b66196954a974fdd72fd8c6425
Liam <ic09272002@yahoo.com.tw>
2018-01-09 01:33:10 +1100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ echo "Upload"
 scp -i ~/.ssh/key/efcsydney.pem ./efcsydney-roster.tar.gz ec2-user@54.206.106.116:/opt/
 
 echo "Replace"
-ssh -i ~/.ssh/key/efcsydney.pem ec2-user@54.206.106.116 "cd /opt; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz"
+ssh -i ~/.ssh/key/efcsydney.pem ec2-user@54.206.106.116 "cd /opt; rm -rf pre-efcsydney-roster; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz"
