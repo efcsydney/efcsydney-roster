9c403a2af0cd19f9e45c117536b9c57b09de6ee7
kleinchang <kleinchang@gmail.com>
2018-01-13 15:31:44 +1100

    specify qa | prod as first parameter

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -x
 
+if [ "$1" = "qa" ]; then
+	ip="54.206.106.116"
+elif [ "$1" = "prod" ]; then
+	ip="13.210.76.160"
+else
+	ip="54.206.106.116"
+fi
+
 echo "Clean up"
 rm -rf client/node_modules
 
@@ -8,7 +16,10 @@ cd ../
 tar -czf efcsydney-roster.tar.gz efcsydney-roster
 
 echo "Upload"
-scp -i ~/.ssh/key/efcsydney.pem ./efcsydney-roster.tar.gz ec2-user@54.206.106.116:/opt/
+scp -i ~/.ssh/key/efcsydney.pem ./efcsydney-roster.tar.gz ec2-user@${ip}:/opt/
 
 echo "Replace"
-ssh -i ~/.ssh/key/efcsydney.pem ec2-user@54.206.106.116 "cd /opt; rm -rf pre-efcsydney-roster; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz"
+ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} "cd /opt; rm -rf pre-efcsydney-roster; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz"
+
+echo "After deploy"
+ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} "cd /opt/efcsydney-roster; . ~/.bash_profile; nvm use 8; yarn"
