9131697e8181862dfb05607652ca7329ddbc623a
kleinchang <kleinchang@gmail.com>
2018-01-15 19:47:57 +1100

    Travis prod config (#88)
    * specify qa | prod as first parameter
    
    * Update .travis.yml
    
    add production ip, pass build mode to deploy.sh
    
    * Resolve conflict: deploy.sh
    
    * Update deploy.sh
    
    set NODE_ENV
    
    * Update deploy.sh
    
    replace double quote with single quote

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -x
 
+if [ "$1" = "qa" ]; then
+	ip="54.206.106.116"
+  export node_env=qa
+elif [ "$1" = "prod" ]; then
+	ip="13.210.76.160"
+  export node_env=production
+else
+	ip="54.206.106.116"
+  export node_env=qa
+fi
+
 echo "Clean up"
 rm -rf client/node_modules
 
@@ -8,7 +19,10 @@ cd ../
 tar -czf efcsydney-roster.tar.gz efcsydney-roster
 
 echo "Upload"
-scp -i ~/.ssh/key/efcsydney.pem ./efcsydney-roster.tar.gz ec2-user@54.206.106.116:/opt/
+scp -i ~/.ssh/key/efcsydney.pem ./efcsydney-roster.tar.gz ec2-user@${ip}:/opt/
 
 echo "Replace"
-ssh -i ~/.ssh/key/efcsydney.pem ec2-user@54.206.106.116 "cd /opt; rm -rf pre-efcsydney-roster; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz"
+ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'export NODE_ENV=${node_env}; cd /opt; rm -rf pre-efcsydney-roster; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz'
+
+echo "After deploy"
+ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'export NODE_ENV=${node_env}; cd /opt/efcsydney-roster; . ~/.bash_profile; nvm use 8; yarn; yarn db-migrate'
