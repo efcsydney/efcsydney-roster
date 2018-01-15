d64f9d0260512b16b54772149e0b25c01627dfd8
josephj <josephj6802@gmail.com>
2018-01-16 08:12:45 +1100

    Hotfix - New Relic path

--- newrelic.js
+++ newrelic.js
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV;
 if (env === 'production' || env === 'qa') {
-  process.env.NEW_RELIC_HOME = `${__dirname}/config/newrelic/${env}.js`;
+  process.env.NEW_RELIC_HOME = `./config/newrelic/${env}`;
   require('newrelic');
 }
