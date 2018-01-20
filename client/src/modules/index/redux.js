663cd3eac8daa721d4c6a561c611400a59a763bd
josephj <josephj6802@gmail.com>
2018-01-21 10:15:10 +1100

    Footnote + Excluded Day - Init Commit

--- client/src/modules/index/redux.js
+++ client/src/modules/index/redux.js
@@ -1,8 +1,11 @@
+import _ from 'lodash';
 import { combineReducers } from 'redux';
 import { createAction, handleAction, handleActions } from 'redux-actions';
 import ServiceInfoAPI from 'apis/serviceInfo';
 import EventsAPI from 'apis/events';
 import store from 'store';
+import moment from 'moment';
+import dotProp from 'dot-prop-immutable';
 
 const PREFIX = 'index';
 
@@ -12,39 +15,88 @@ export const receiveRetrieveEvents = createAction(
 export const requestRetrieveEvents = createAction(
   `${PREFIX}/REQUEST_RETRIEVE_EVENTS`,
   payload => {
-    EventsAPI.retrieve(payload).then(data =>
+    EventsAPI.retrieve(payload).then(({ data }) =>
       store.dispatch(receiveRetrieveEvents(data))
     );
     return payload;
   }
 );
+export const receiveModifyIdEvents = createAction(
+  `${PREFIX}/RECEIVE_MODIFY_ID_EVENTS`
+);
+export const requestModifyIdEvents = createAction(
+  `${PREFIX}/REQUEST_MODIFY_ID_EVENTS`,
+  payload => {
+    EventsAPI.modify(payload).then(() => {
+      store.dispatch(receiveModifyIdEvents(payload));
+    });
+    return payload;
+  }
+);
 export const receiveModifyServiceInfo = createAction(
-  `${PREFIX}/REQUEST_MODIFY_SERVICE_INFO`
+  `${PREFIX}/RECEIVE_MODIFY_SERVICE_INFO`
 );
 export const requestModifyServiceInfo = createAction(
   `${PREFIX}/REQUEST_MODIFY_SERVICE_INFO`,
   payload => {
-    const { id, ...body } = payload;
-    ServiceInfoAPI.modify(id, body).then(data =>
-      store.dispatch(receiveModifyServiceInfo(data))
+    let { id, ...body } = payload;
+    body = dotProp.set(body, 'footnote', body.footnote.trim());
+    body = dotProp.set(body, 'skipReason', body.skipReason.trim());
+    body = dotProp.set(body, 'skipService', !!body.skipReason);
+    ServiceInfoAPI.modify(id, body).then(() =>
+      store.dispatch(
+        receiveModifyServiceInfo({ id, serviceInfo: { id, ...body } })
+      )
     );
     return payload;
   }
 );
+export const setSelectedData = createAction(`${PREFIX}/SET_SELECTED_DATA`);
+export const toggleEditRole = createAction(`${PREFIX}/TOGGLE_EDIT_ROLE`);
+export const toggleEditDay = createAction(`${PREFIX}/TOGGLE_EDIT_DAY`);
 
 const defaultState = {
-  data: {},
+  data: [],
   meta: {
-    query: {},
+    isEditingDay: false,
+    isEditingRole: false,
     isLoading: false,
-    isSaving: false
+    isSaving: false,
+    query: {},
+    selectedData: null
   }
 };
 
 export default combineReducers({
-  data: handleAction(
-    receiveRetrieveEvents,
-    (state, { payload }) => payload,
+  data: handleActions(
+    {
+      [receiveRetrieveEvents]: (state, { payload }) => payload,
+      [receiveModifyIdEvents]: (state, { payload }) => {
+        const dayIndex = _.findIndex(state, {
+          date: moment(payload.date).format('YYYY-MM-DD')
+        });
+        const roleIndex = _.findIndex(state[dayIndex].members, {
+          role: payload.role
+        });
+
+        return dotProp.set(
+          state,
+          `${dayIndex}.members.${roleIndex}.name`,
+          payload.name
+        );
+      },
+      [receiveModifyServiceInfo]: (state, { payload }) => {
+        const dayIndex = _.findIndex(state, day => {
+          const id = _.get(day, 'serviceInfo.id', null);
+          return id === payload.id;
+        });
+        return dotProp.set(
+          state,
+          `${dayIndex}.serviceInfo`,
+          payload.serviceInfo
+        );
+      }
+    },
     defaultState.data
   ),
   meta: combineReducers({
@@ -52,6 +104,22 @@ export default combineReducers({
       requestRetrieveEvents,
       (state, { payload }) => payload,
       defaultState.meta.query
+    ),
+    isEditingDay: handleActions(
+      {
+        [toggleEditDay]: (state, { payload }) =>
+          _.isBoolean(payload) ? payload : !state,
+        [receiveModifyServiceInfo]: () => false
+      },
+      defaultState.meta.isEditingDay
+    ),
+    isEditingRole: handleActions(
+      {
+        [toggleEditRole]: (state, { payload }) =>
+          _.isBoolean(payload) ? payload : !state,
+        [receiveModifyIdEvents]: () => false
+      },
+      defaultState.meta.isEditingRole
     ),
     isLoading: handleActions(
       {
@@ -62,10 +130,28 @@ export default combineReducers({
     ),
     isSaving: handleActions(
       {
+        [requestModifyIdEvents]: () => true,
+        [receiveModifyIdEvents]: () => false,
         [requestModifyServiceInfo]: () => true,
         [receiveModifyServiceInfo]: () => false
       },
       defaultState.meta.isSaving
+    ),
+    selectedData: handleActions(
+      {
+        [toggleEditDay]: (state, { payload }) => {
+          const isClosing =
+            payload === false || (state && _.isUndefined(payload));
+          return isClosing ? null : state;
+        },
+        [toggleEditRole]: (state, { payload }) => {
+          const isClosing =
+            payload === false || (state && _.isUndefined(payload));
+          return isClosing ? null : state;
+        },
+        [setSelectedData]: (state, { payload }) => payload
+      },
+      defaultState.meta.selectedData
     )
   })
 });
