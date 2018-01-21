50e08c50302284fe77f33f1a9c44de5ba62ad8c2
Liam <ic09272002@yahoo.com.tw>
2018-01-21 23:21:56 +1100

    Add monit script

--- deploy.sh
+++ deploy.sh
@@ -26,3 +26,6 @@ ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'export NODE_ENV=${node_env}; cd 
 
 echo "After deploy"
 ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'export NODE_ENV=${node_env}; cd /opt/efcsydney-roster; . ~/.bash_profile; nvm use 8; yarn; yarn db-migrate'
+
+echo "Restart node process"
+ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'sudo monit restart all'
