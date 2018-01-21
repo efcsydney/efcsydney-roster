9c74dc1097acd1ecd02d5376ffa7f4976242da30
Liam <ic09272002@yahoo.com.tw>
2018-01-21 23:39:38 +1100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,21 +11,21 @@ else
   export node_env=qa
 fi
 
-echo "Clean up"
+echo "Clean up Useless files"
 rm -rf client/node_modules
 
-echo "Tar"
+echo "Compress all the code into tarball"
 cd ../
 tar -czf efcsydney-roster.tar.gz efcsydney-roster
 
-echo "Upload"
+echo "Upload Tarball files to server under /opt"
 scp -i ~/.ssh/key/efcsydney.pem ./efcsydney-roster.tar.gz ec2-user@${ip}:/opt/
 
-echo "Replace"
+echo "Replace existed code to /opt/pre-efcsydney-roster and untar current code into /opt/efcsydney-roster"
 ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'export NODE_ENV=${node_env}; cd /opt; rm -rf pre-efcsydney-roster; mv efcsydney-roster pre-efcsydney-roster; tar -xf efcsydney-roster.tar.gz'
 
-echo "After deploy"
+echo "Run DB migrate"
 ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'export NODE_ENV=${node_env}; cd /opt/efcsydney-roster; . ~/.bash_profile; nvm use 8; yarn; yarn db-migrate'
 
-echo "Restart node process"
+echo "Restart Node Process Using Monit, Config in the /etc/monit.d/roster"
 ssh -i ~/.ssh/key/efcsydney.pem ec2-user@${ip} 'sudo monit restart all'
