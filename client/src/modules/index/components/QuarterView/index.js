9ecc282d5a8020c5233d0b6bb234636b4915ca66
Jeffreyx <jeffreyx@gmail.com>
2018-01-26 18:20:27 +1100

    don't overflow scroll the calendar in mobile view

--- client/src/modules/index/components/QuarterView/index.js
+++ client/src/modules/index/components/QuarterView/index.js
@@ -80,7 +80,8 @@ export default class QuarterView extends Component {
         ref={input => {
           this.calendarWrapper = input;
         }}
-        calendarHeight={calendarHeight}>
+        calendarHeight={calendarHeight}
+        isMobile={isMobile}>
         {isMobile && <Mobile {...viewProps} />}
         {!isMobile && <Desktop {...viewProps} />}
       </Wrapper>
@@ -95,7 +96,7 @@ const Wrapper = styled.div`
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
   font-size: 13px;
   position: absolute;
-  overflow: scroll;
+  overflow: ${props => props.isMobile ? 'inherit' : 'scroll'} ;
   width: 100%;
   max-height: ${props => props.calendarHeight};
 `;
