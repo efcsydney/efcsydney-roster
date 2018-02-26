7dc600307a58a5e3f01c4f03dc43db67a9b2c52a
josephj <josephj6802@gmail.com>
2018-02-27 07:47:30 +1100

    Edit Day > Skip Service - Switch Button

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,14 @@ import DateBar from './DateBar';
 import LoadingIndicator from './LoadingIndicator';
 import Modal from './Modal';
 import StateButton from './StateButton';
+import SwitchButton from './SwitchButton';
 import TagManager from './TagManager';
 
-export { DateBar, LoadingIndicator, Modal, StateButton, TagManager };
+export {
+  DateBar,
+  LoadingIndicator,
+  Modal,
+  StateButton,
+  SwitchButton,
+  TagManager
+};
