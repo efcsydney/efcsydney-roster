f63b211d0ef43faa8f5d3fa5e5e7275036afff0c
josephj <josephj6802@gmail.com>
2018-03-14 23:16:24 +1100

    Add Edit Modal

---
+++ client/src/modules/admin/Edit.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { Modal, StateButton } from 'components';
+import styled from 'styled-components';
+
+export default class Edit extends Component {
+  static propTypes = {
+    data: PropTypes.object,
+    onClose: PropTypes.func
+  };
+  static defaultProps = {
+    data: {},
+    onClose: () => {}
+  };
+  render() {
+    let {
+      data: { label, frequency, footnoteLabel, positions },
+      onClose
+    } = this.props;
+    positions = positions || [];
+    return (
+      <Modal isOpen={true} title="Edit Service" onClose={onClose}>
+        <Form>
+          <Row>
+            <Label>Frequency</Label>
+            <span>{frequency}</span>
+          </Row>
+          <Row>
+            <Label>Service Title</Label>
+            <span>{label}</span>
+          </Row>
+          <Row>
+            <Label>Footnote</Label>
+            <span>{footnoteLabel}</span>
+          </Row>
+          <Row>
+            <Label>Positions</Label>
+            <span>
+              <ul>
+                {positions.map(({ id, name }) => <li key={id}>{name}</li>)}
+              </ul>
+            </span>
+          </Row>
+          <Row align="center">
+            <StateButton type="submit">Save</StateButton>
+          </Row>
+        </Form>
+      </Modal>
+    );
+  }
+}
+
+const Form = styled.form`
+  display: table;
+  margin: 0 auto;
+  position: relative;
+`;
+Form.displayName = 'Form';
+
+const Row = styled.div`
+  align-items: center;
+  display: flex;
+  min-height: 50px;
+  &:last-child {
+    text-align: center;
+    padding: 20px 0;
+  }
+  justify-content: ${props => props.align || 'flex-start'};
+`;
+Row.displayName = 'Row';
+
+const Label = styled.label`
+  display: inline-block;
+  font-weight: bold;
+  padding-right: 15px;
+  text-align: right;
+  width: 125px;
+`;
+Label.displayName = 'Label';
--- client/src/modules/admin/index.js
+++ client/src/modules/admin/index.js
@@ -1,73 +1,98 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ServicesAPI from 'apis/services';
 import { Auth, NavBar } from 'modules/core';
 import styled from 'styled-components';
-import { Button, Grid, Row, Cell, HeaderCell } from 'components';
+import { Button, Cell, Grid, HeaderCell, Row } from 'components';
 import { Link } from 'react-router-dom';
 import IconPencil from 'react-icons/lib/fa/pencil';
-export default class AdminIndex extends Component {
-  state = {
-    data: []
-  };
-  handleAuthFail = () => {
-    const { history } = this.props;
+import Edit from './Edit';
 
-    history.replace('login');
+const mapStateToProps = (state, ownProps) => {
+  const selectedId = _.get(ownProps, 'match.params.id', null);
+  return {
+    selectedId: parseInt(selectedId, 10)
   };
-  handleAuthSuccess = () => {
-    ServicesAPI.retrieve().then(({ data }) => {
-      this.setState({ data });
-    });
-  };
-  render() {
-    const { data } = this.state;
-    return (
-      <Wrapper>
-        <NavBar hasSwitcher={false} title="Roster System" />
-        <Body>
-          <Auth onFail={this.handleAuthFail} onSuccess={this.handleAuthSuccess}>
-            <Grid>
-              <thead>
-                <Row>
-                  <HeaderCell>ID</HeaderCell>
-                  <HeaderCell>Service Title</HeaderCell>
-                  <HeaderCell>Positions</HeaderCell>
-                  <HeaderCell>Footnote Label</HeaderCell>
-                  <HeaderCell>Occurrence</HeaderCell>
-                  <HeaderCell>Actions</HeaderCell>
-                </Row>
-              </thead>
-              <tbody>
-                {data.map(
-                  ({ footnoteLabel, frequency, label, id, positions }) => (
-                    <Row>
-                      <Cell>{id}</Cell>
-                      <Cell>
-                        <Link to={`/admin/edit/${id}`}>{label}</Link>
-                      </Cell>
-                      <Cell>{positions.length}</Cell>
-                      <Cell>{footnoteLabel}</Cell>
-                      <Cell>{_.capitalize(frequency)}</Cell>
-                      <Cell>
-                        <Link to={`/admin/edit/${id}`}>
-                          <Button kind="blue">
-                            <IconEdit />
-                            Edit
-                          </Button>
-                        </Link>
-                      </Cell>
-                    </Row>
-                  )
-                )}
-              </tbody>
-            </Grid>
-          </Auth>
-        </Body>
-      </Wrapper>
-    );
+};
+export default connect(mapStateToProps)(
+  class AdminIndex extends Component {
+    state = {
+      data: []
+    };
+    handleAuthFail = () => {
+      const { history } = this.props;
+
+      history.replace('login');
+    };
+    handleAuthSuccess = () => {
+      ServicesAPI.retrieve().then(({ data }) => {
+        this.setState({ data });
+      });
+    };
+    handleEditClose = () => {
+      const { history } = this.props;
+
+      history.push('/admin');
+    };
+    render() {
+      const { data } = this.state;
+      const { selectedId } = this.props;
+      return (
+        <Wrapper>
+          <NavBar hasSwitcher={false} title="Roster System" />
+          <Body>
+            <Auth
+              onFail={this.handleAuthFail}
+              onSuccess={this.handleAuthSuccess}>
+              <Grid>
+                <thead>
+                  <Row>
+                    <HeaderCell>ID</HeaderCell>
+                    <HeaderCell>Service Title</HeaderCell>
+                    <HeaderCell>Positions</HeaderCell>
+                    <HeaderCell>Footnote Label</HeaderCell>
+                    <HeaderCell>Occurrence</HeaderCell>
+                    <HeaderCell>Actions</HeaderCell>
+                  </Row>
+                </thead>
+                <tbody>
+                  {data.map(
+                    ({ footnoteLabel, frequency, label, id, positions }) => (
+                      <Row key={id}>
+                        <Cell>{id}</Cell>
+                        <Cell>
+                          <Link to={`/admin/edit/${id}`}>{label}</Link>
+                        </Cell>
+                        <Cell>{positions.length}</Cell>
+                        <Cell>{footnoteLabel}</Cell>
+                        <Cell>{_.capitalize(frequency)}</Cell>
+                        <Cell>
+                          <Link to={`/admin/edit/${id}`}>
+                            <Button kind="blue">
+                              <IconEdit />
+                              Edit
+                            </Button>
+                          </Link>
+                        </Cell>
+                      </Row>
+                    )
+                  )}
+                </tbody>
+              </Grid>
+            </Auth>
+          </Body>
+          {!!(selectedId && _.isNumber(selectedId)) && (
+            <Edit
+              data={_.find(data, { id: selectedId })}
+              onClose={this.handleEditClose}
+            />
+          )}
+        </Wrapper>
+      );
+    }
   }
-}
+);
 
 const Wrapper = styled.div``;
 const Body = styled.div`
