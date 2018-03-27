29898efa0c802113122c8d61918139da7e5c3887
KyleH <yhua045@gmail.com>
2018-03-27 21:00:59 +1100

    Refactor the code by removed duplicate code blocks as per CodeClimate's suggestions

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const app = require('./app').app;
 const logger = require('./api/utilities/logger');
 const databaseUtil = require('./api/utilities/database-util');
 
-//listen for non secure port
-app.listen(app.get('port'), () => {
-  logger.info(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
-});
-
-//listen for secure port
-app.listen(app.get('secure-port'), () => {
-  logger.info(`Find the server at: https://localhost:${app.get('secure-port')}/`); // eslint-disable-line no-console
+const ports = [app.get('port'), app.get('secure-port')];
+ports.forEach(port => {
+  app.listen(port, () => {
+    logger.info(`Find the server at: http://localhost:${port}/`);
+  })
 });
 
 // cron job for scheduled email
