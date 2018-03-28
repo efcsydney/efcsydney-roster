de1ba76c9d4ca64fc17c850193a025b1dd5dcc96
Kyle Huang <yhua045@gmail.com>
2018-03-28 18:16:43 +1100

    Admin > Login - Error Page (#181)
    * Added error response when login has failed
    
    * Update the code as per review's comment
    
    * Update handleBackClick event and redirect the user back to home page
    
    * Update the icon and change the 'no permission' text
    
    * Refactor the code

--- client/src/modules/login/index.js
+++ client/src/modules/login/index.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
 import { Auth, NavBar } from 'modules/core';
+import Button from 'components/Button';
 import styled from 'styled-components';
 import firebase from 'firebase';
 import { injectGlobal } from 'styled-components';
+import IconInfo from 'react-icons/lib/md/info-outline';
 
 export default class Login extends Component {
-  handleAuthSuccess = () => {
+  constructor(props) {
+    super(props);
+    this.state = { loginError: false };
+  }
+  redirectTo = (page) => {
     const { history } = this.props;
-
-    history.replace('/admin');
+    history.replace(page);
+  }
+  handleAuthSuccess = () => {
+    this.setState({ loginError: false });
+    this.redirectTo('/admin');
   };
+  handleAuthFail = (reason) => {
+    if (reason === 'no-permission') {
+      this.setState({ loginError: true });
+    }
+  }
   handleLogin = () => {
     const provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithPopup(provider);
   };
+  handleBackClick = () => {
+    this.setState({ loginError: false });
+    this.redirectTo('/');
+  }
+  renderLoginError = () => {
+    return (
+      <Wrapper style={{maxWidth: '50%'}}>
+        <SadFaceIcon />
+        <Title>Almost there!</Title>
+        <P>You have logged in successfully.<br /><br />However the System Administartor needs to review and grant your account permission before you can access Admin Portal. Please contact the system administartor for further assistance.</P>
+        <Button style={{maxWidth: '200px', margin: '20px auto'}} onClick={this.handleBackClick}>Go to Home</Button>
+      </Wrapper>
+    );
+  }
+  renderLoginButton = () => {
+    return (
+      <Auth onSuccess={this.handleAuthSuccess} onFail={this.handleAuthFail}>
+        <LoginButton onClick={this.handleLogin} />
+      </Auth>
+    );
+  }
+
   render() {
+    const { loginError } = this.state;
     return (
       <Wrapper>
         <NavBar hasSwitcher={false} title="Roster System" />
         <Body>
-          <Auth onSuccess={this.handleAuthSuccess}>
-            <LoginButton onClick={this.handleLogin} />
-          </Auth>
+          {loginError && this.renderLoginError()}
+          {!loginError && this.renderLoginButton()}
         </Body>
       </Wrapper>
     );
@@ -40,6 +76,21 @@ const LoginButton = styled.a`
   cursor: pointer;
   width: 247px;
   height: 40px;
+`;
+const Title = styled.h1`
+  font-size: 200%;
+  color: #2d5faf;
+  text-align: center;
+`;
+const P = styled.p`
+  font-size: 1.5em;
+  text-align: center;
+`;
+const SadFaceIcon = styled(IconInfo) `
+  display: block;
+  margin: 0 auto;
+  width:250px;
+  font-size: 1000%;
 `;
 const Body = styled.div`
   align-items: center;
