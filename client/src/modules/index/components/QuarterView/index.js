8b1877914a9c07330a4b59184c4bc4df2615850a
Choco <trust2065@example.com>
2018-05-03 16:33:15 +1000

    Ensure that the scrollbars don’t show on the Windows browsers

--- client/src/modules/index/components/QuarterView/index.js
+++ client/src/modules/index/components/QuarterView/index.js
@@ -101,7 +101,7 @@ const Wrapper = styled.div`
   box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.1);
   font-size: 13px;
   position: absolute;
-  overflow: ${props => (props.isMobile ? 'inherit' : 'scroll')};
+  overflow: ${props => (props.isMobile ? 'inherit' : 'auto')};
   width: 100%;
   max-height: ${props => (props.isMobile ? 'none' : props.calendarHeight)};
   ${media.print`
