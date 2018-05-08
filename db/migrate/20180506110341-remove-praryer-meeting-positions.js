f206df5ee8b3b9c4e8e535e94beaf2c2f87ba1f5
KyleH <yhua045@gmail.com>
2018-05-08 20:22:03 +1000

    #246: Added migration scrip to clean up Content and Title positions from Prayer service

---
+++ db/migrate/20180506110341-remove-praryer-meeting-positions.js
@@ -0,0 +1,28 @@
+const sequelizeClient = require('../../api/infrastructure/sequelize-client')
+  .sequelizeClient;
+const _ = require('lodash');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    const servicesData = (await sequelizeClient.query(
+      `SELECT id, name from services WHERE name IN ('prayer')`
+    ))[0];
+
+    const prayerService = _.find(servicesData, { name: 'prayer' });
+    //Remove Topic and Content positions.
+    await sequelizeClient.query(
+      `DELETE FROM positions WHERE serviceId = ${prayerService.id} AND name IN ('Topic','Content')`
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
