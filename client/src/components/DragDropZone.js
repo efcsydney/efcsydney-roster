46cf442ab6849903d500f1d3d35bf99244fffa4d
Choco <trust2065@example.com>
2018-05-20 21:22:02 +1000

    create empty component from DragDropContext

--- client/src/components/DragDropZone.js
+++ client/src/components/DragDropZone.js
@@ -1,8 +1,13 @@
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import styled from 'styled-components';
+import { Component } from 'react';
 
-const DragDropZone = DragDropContext(HTML5Backend)(styled.div``);
-DragDropZone.displayName = 'DragDropZone';
+const DragDropZone = DragDropContext(HTML5Backend)(
+  class DragDropZone extends Component {
+    render() {
+      return this.props.children;
+    }
+  }
+);
 
 export default DragDropZone;
