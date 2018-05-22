015f246f48fc7fbe4da65e51341983c5fe20bea1
LiamLai <liam.lai@tabcorp.com.au>
2018-05-22 22:08:16 +1000

    only info level up of log are writing to log file

--- api/utilities/logger.js
+++ api/utilities/logger.js
@@ -1,29 +1,13 @@
 const winston = require('winston');
 const winstonDailyRotateFile = require('winston-daily-rotate-file');
 winston.emitErrs = true;
+winston.level = 'error';
 /*
 * The source code is copied from the posts : http://tostring.it/2014/06/23/advanced-logging-with-nodejs/ and
 * http://thisdavej.com/using-winston-a-versatile-logging-library-for-node-js/
 */
 const tsFormat = () => new Date().toISOString();
-const errorLogTransport = new winstonDailyRotateFile({
-  name: 'error',
-  level: 'error',
-  filename: 'log/.log',
-  timestamp: tsFormat,
-  datePattern: 'yyyy-MM-dd',
-  maxFiles: '14d',
-  prepend: true
-});
-const debugLogTransport = new winstonDailyRotateFile({
-  name: 'debug',
-  level: 'debug',
-  filename: 'log/.log',
-  timestamp: tsFormat,
-  datePattern: 'yyyy-MM-dd',
-  maxFiles: '14d',
-  prepend: true
-});
+
 const infoLogTransport = new winstonDailyRotateFile({
   name: 'info',
   level: 'info',
@@ -41,7 +25,7 @@ const consoleTransport = new winston.transports.Console({
 });
 
 const logger = new winston.Logger({
-  transports: [errorLogTransport, debugLogTransport, infoLogTransport, consoleTransport],
+  transports: [infoLogTransport, consoleTransport],
   exitOnError: false
 });
 
