f880b6a68e27c9fb690c66420815ca31373ac364
josephj <josephj6802@gmail.com>
2018-05-26 09:23:14 +1000

    #286 Fix: Customise EventsAPI.modify

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,5 +1,15 @@
 import { createApi } from './utils';
 
-export const EventsAPI = createApi('/api/events');
+export const EventsAPI = createApi('/api/events', {
+  modify: body => {
+    return fetch('/api/events', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }).then(response => response.json());
+  }
+});
 export const ServicesAPI = createApi('/api/services');
 export const ServiceInfoAPI = createApi('/api/serviceInfo');
