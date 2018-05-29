aff59bdfe024904d675e4f5814f38ea72e86b8bf
Liam <ic09272002@yahoo.com.tw>
2018-05-29 21:32:42 +1000

    correct log module and add some log (#298)

--- api/utilities/logger.js
+++ api/utilities/logger.js
@@ -1,25 +1,20 @@
 const winston = require('winston');
 const winstonDailyRotateFile = require('winston-daily-rotate-file');
 winston.emitErrs = true;
+
 /*
 * The source code is copied from the posts : http://tostring.it/2014/06/23/advanced-logging-with-nodejs/ and
 * http://thisdavej.com/using-winston-a-versatile-logging-library-for-node-js/
 */
-const tsFormat = () => new Date().toLocaleTimeString();
-const errorLogTransport = new winstonDailyRotateFile({
-  name: 'error',
-  level: 'error',
-  filename: 'log/-error.log',
+const tsFormat = () => new Date().toISOString();
+
+const infoLogTransport = new winstonDailyRotateFile({
+  name: 'info',
+  level: 'info',
+  filename: 'log/.log',
   timestamp: tsFormat,
   datePattern: 'yyyy-MM-dd',
-  prepend: true
-});
-const debugLogTransport = new winstonDailyRotateFile({
-  name: 'debug',
-  level: 'debug',
-  filename: 'log/-log.log',
-  timestamp: tsFormat,
-  datePattern: 'yyyy-MM-dd',
+  maxFiles: '14d',
   prepend: true
 });
 const consoleTransport = new winston.transports.Console({
@@ -30,7 +25,7 @@ const consoleTransport = new winston.transports.Console({
 });
 
 const logger = new winston.Logger({
-  transports: [errorLogTransport, debugLogTransport, consoleTransport],
+  transports: [infoLogTransport, consoleTransport],
   exitOnError: false
 });
 
