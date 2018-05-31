7b6424bb893749ba0b0789b2de7f01130fa9d3ea
josephj <josephj6802@gmail.com>
2018-05-31 11:45:12 +0800

    #297 Update the Admin Services page with withResource HoC

--- client/src/modules/admin/services/Container.js
+++ client/src/modules/admin/services/Container.js
@@ -1,16 +1,17 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { ServicesAPI } from 'apis';
+import { PropTypes } from 'prop-types';
 import { Auth, NavBar } from 'modules/core';
 import styled from 'styled-components';
 import { Button, Cell, Grid, HeaderCell, Row } from 'components';
 import { Link } from 'react-router-dom';
 import IconPencil from 'react-icons/lib/fa/pencil';
-import { withResource } from 'resource/connect';
 import Popup from './Popup';
+import { createApiActions, withResource } from 'resource';
 
-const mapStateToProps = (state, ownProps) => {
+const { retrieveService } = createApiActions('services');
+
+const mapResourceToProps = (resource, state, ownProps) => {
   const path = _.get(ownProps, 'match.path');
   const mode =
     (path.indexOf('/new') !== -1 && 'new') ||
@@ -18,122 +19,105 @@ const mapStateToProps = (state, ownProps) => {
     'index';
   const paramId = _.get(ownProps, 'match.params.id', null);
   const selectedId = !_.isEmpty(paramId) ? parseInt(paramId, 10) : null;
+  const data = resource.data;
 
   return {
+    data,
     mode,
-    selectedId
+    selectedId,
+    ...ownProps
   };
 };
-export default withResource('services')(
-  connect(mapStateToProps)(
-    class AdminIndex extends Component {
-      state = {
-        data: []
-      };
-      constructor(props) {
-        super(props);
+export default withResource('services', mapResourceToProps)(
+  class AdminIndex extends Component {
+    static propTypes = {
+      data: PropTypes.object,
+      mode: PropTypes.string,
+      selectedId: PropTypes.number
+    };
+    constructor(props) {
+      super(props);
 
-        this.rootPath = '/admin/services';
-      }
-      handleAuthFail = () => {
-        const { history } = this.props;
-
-        history.replace('/login');
-      };
-      handleAuthSuccess = () => {
-        ServicesAPI.retrieve().then(({ data }) => {
-          this.setState({ data });
-        });
-      };
-      handlePopupClose = () => {
-        const { history } = this.props;
-
-        history.push(this.rootPath);
-      };
-      handlePopupSave = data => {
-        const { history } = this.props;
-        const { data: prevData } = this.state;
-        const nextData = _.clone(prevData);
-
-        const { id, ...body } = data;
-        const offset = _.findIndex(prevData, { id: id });
-
-        ServicesAPI.modify(id, body)
-          .then(({ data }) => {
-            nextData[offset] = data;
-            this.setState({ data: nextData });
-          })
-          .catch(e => alert(e.message))
-          .finally(() => history.push(this.rootPath));
-      };
-      render() {
-        const { data } = this.state;
-        const { mode, selectedId } = this.props;
-        const hasPopup = _.includes(['new', 'edit'], mode);
-        const selectedData = _.find(data, { id: selectedId }) || {};
-        const isLoading = mode === 'edit' && _.isEmpty(selectedData);
-
-        return (
-          <Wrapper>
-            <NavBar hasSwitcher={false} title="Roster System" />
-            <Body>
-              <Auth
-                onFail={this.handleAuthFail}
-                onSuccess={this.handleAuthSuccess}>
-                <HeadRow>
-                  <Link to={`${this.rootPath}/new`}>
-                    <Button kind="green" theme="solid">
-                      Create New Service
-                    </Button>
-                  </Link>
-                </HeadRow>
-                <Grid>
-                  <thead>
-                    <Row>
-                      <HeaderCell>Service Title</HeaderCell>
-                      <HeaderCell>Positions</HeaderCell>
-                      <HeaderCell>Frequency</HeaderCell>
-                      <HeaderCell>Actions</HeaderCell>
-                    </Row>
-                  </thead>
-                  <tbody>
-                    {data.map(({ frequency, label, id, positions }) => (
-                      <Row key={id}>
-                        <Cell>
-                          <Link to={`${this.rootPath}/edit/${id}`}>
-                            {label}
-                          </Link>
-                        </Cell>
-                        <Cell>{positions.length}</Cell>
-                        <Cell>{_.capitalize(frequency)}</Cell>
-                        <Cell>
-                          <Link to={`${this.rootPath}/edit/${id}`}>
-                            <Button kind="blue">
-                              <IconEdit />
-                              Edit
-                            </Button>
-                          </Link>
-                        </Cell>
-                      </Row>
-                    ))}
-                  </tbody>
-                </Grid>
-              </Auth>
-            </Body>
-            {hasPopup && (
-              <Popup
-                mode={mode}
-                data={selectedData}
-                isLoading={isLoading}
-                onSave={this.handlePopupSave}
-                onClose={this.handlePopupClose}
-              />
-            )}
-          </Wrapper>
-        );
-      }
+      this.rootPath = '/admin/services';
     }
-  )
+    handleAuthFail = () => {
+      const { history } = this.props;
+
+      history.replace('/login');
+    };
+    handlePopupClose = () => {
+      const { history } = this.props;
+
+      history.push(this.rootPath);
+    };
+    handlePopupSave = data => {
+      const { dispatch } = this.props;
+      const { id, ...body } = data;
+
+      dispatch(retrieveService(id, body));
+    };
+    render() {
+      const { data, mode, selectedId } = this.props;
+      const hasPopup = _.includes(['new', 'edit'], mode);
+      const selectedData = data[selectedId] || {};
+      const isLoading = mode === 'edit' && _.isEmpty(selectedData);
+
+      return (
+        <Wrapper>
+          <NavBar hasSwitcher={false} title="Roster System" />
+          <Body>
+            <Auth onFail={this.handleAuthFail}>
+              <HeadRow>
+                <Link to={`${this.rootPath}/new`}>
+                  <Button kind="green" theme="solid">
+                    Create New Service
+                  </Button>
+                </Link>
+              </HeadRow>
+              <Grid>
+                <thead>
+                  <Row>
+                    <HeaderCell>Service Title</HeaderCell>
+                    <HeaderCell>Positions</HeaderCell>
+                    <HeaderCell>Frequency</HeaderCell>
+                    <HeaderCell>Actions</HeaderCell>
+                  </Row>
+                </thead>
+                <tbody>
+                  {_.map(data, ({ frequency, label, id, positions }, key) => (
+                    <Row key={key}>
+                      <Cell>
+                        <Link to={`${this.rootPath}/edit/${id}`}>{label}</Link>
+                      </Cell>
+                      <Cell>{positions.length}</Cell>
+                      <Cell>{_.capitalize(frequency)}</Cell>
+                      <Cell>
+                        <Link to={`${this.rootPath}/edit/${id}`}>
+                          <Button kind="blue">
+                            <IconEdit />
+                            Edit
+                          </Button>
+                        </Link>
+                      </Cell>
+                    </Row>
+                  ))}
+                </tbody>
+              </Grid>
+            </Auth>
+          </Body>
+          {hasPopup && (
+            <Popup
+              mode={mode}
+              data={selectedData}
+              isLoading={isLoading}
+              onSave={this.handlePopupSave}
+              onClose={this.handlePopupClose}
+            />
+          )}
+        </Wrapper>
+      );
+    }
+  }
 );
 
 const Wrapper = styled.div``;
