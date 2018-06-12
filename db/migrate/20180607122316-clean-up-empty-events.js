3c05ab76fbf628373c212a60494dcf329142e06e
KyleH <yhua045@gmail.com>
2018-06-12 23:12:18 +1000

    #247: Create migration script to remove empty events

--- db/migrate/20180607122316-clean-up-empty-events.js
+++ db/migrate/20180607122316-clean-up-empty-events.js
@@ -1,23 +1,40 @@
-'use strict';
+const sequelizeClient = require('../../api/infrastructure/sequelize-client')
+  .sequelizeClient;
+const _ = require('lodash');
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+  up: async () => {
+    await sequelizeClient.query("DELETE FROM events WHERE volunteerName = ' '");
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+  down: async queryInterface => {
+    const calendarDatesData = (await sequelizeClient.query(
+      'SELECT id from calendar_dates'
+    ))[0];
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    // Build positions mapper, which looks like: { 'Speaker': 1 }
+    const positionsData = (await sequelizeClient.query(
+      'SELECT id from positions'
+    ))[0];
+
+    const eventsInDb = (await sequelizeClient.query('SELECT * from events'))[0];
+
+    const events = [];
+    calendarDatesData.forEach(calendarDate => {
+      positionsData.forEach(position => {
+        const eventAlreadyExits = _.find(eventsInDb, function(o) {
+          return (
+            o.calendarDateId === calendarDate.id && o.positionId === position.id
+          );
+        });
+        if (!eventAlreadyExits) {
+          events.push({
+            calendarDateId: calendarDate.id,
+            positionId: position.id
+          });
+        }
+      });
+    });
+    return queryInterface.bulkInsert('events', events);
   }
 };
