50e80eb1f0d49e7a701202c76d022b7b9c6f0a2e
josephj <josephj6802@gmail.com>
2018-06-15 14:09:53 +1000

    #317 Fix: Local not working correctly

--- client/src/modules/core/sagas.js
+++ client/src/modules/core/sagas.js
@@ -1,17 +1,14 @@
 import _ from 'lodash';
-import { takeEvery, call, put, select } from 'redux-saga/effects';
-import {
-  retrieveServices,
-  retrieveServicesComplete,
-  setMeta,
-  switchCategory
-} from './redux';
-import { ServicesAPI } from 'apis';
+import { takeEvery, put, select } from 'redux-saga/effects';
+import { setMeta, switchCategory } from './redux';
 import Cookies from 'js-cookie';
 import moment from 'moment';
 import 'moment/locale/en-au';
 import 'moment/locale/zh-tw';
 import i18n from 'i18n';
+import { createApiActions } from 'resource';
+
+const { retrieveServicesComplete } = createApiActions('services');
 
 function* setLocale(locale) {
   moment.locale(locale);
@@ -20,28 +17,21 @@ function* setLocale(locale) {
 }
 
 export default function* coreSagas() {
-  yield takeEvery(retrieveServices.toString(), function*() {
-    try {
-      const data = yield call(ServicesAPI.retrieve);
-      const services = data.data;
-      yield put(retrieveServicesComplete(services));
-
-      const state = yield select();
-      const category = _.get(state, 'core.meta.category');
-      const selectedService = _.find(services, { name: category });
-
-      if (selectedService) {
-        const { locale } = selectedService;
-        yield setLocale(locale);
-      }
-    } catch (error) {
-      yield put(retrieveServicesComplete(error));
+  yield setLocale('en-AU');
+  yield takeEvery(retrieveServicesComplete.toString(), function*({ payload }) {
+    const services = payload.data;
+    const state = yield select();
+    const category = _.get(state, 'core.meta.category');
+    const selectedService = _.find(services, { name: category });
+    if (selectedService) {
+      const locale = selectedService.locale;
+      yield setLocale(locale);
     }
   });
 
   yield takeEvery(switchCategory.toString(), function*({ payload }) {
     const state = yield select();
-    const services = _.get(state, 'core.data.services', []);
+    const services = _.get(state, 'resource.data.services', {});
     const selectedService = _.find(services, { name: payload });
 
     if (selectedService) {
