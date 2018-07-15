bb691db2c1f57bc460963375e74612a74d5ef621
LiamLai <liam.lai@tabcorp.com.au>
2018-07-15 21:34:35 +1000

    using travis to deploy

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,6 +1,29 @@
+#!/bin/sh
+# create parameter
+if [ $1 = "qa" ]; then
+    env='QA'
+    NodeEnv='qa'
+    bucket='efc-sydney-qa'
+    profile=efc-qa
+else
+    env='Production'
+    NodeEnv='production'
+    #bucket='efc-sydney-prod'
+fi
 
-#file="./ecs_alb.json"
-#stack=ecs-alb
-file="./ecs_service.json"
-stack=ecs-service
-aws --profile efc-qa cloudformation deploy --template-file $file --stack-name $stack --capabilities CAPABILITY_IAM
+docker-compose -f docker/docker-compose.yml build efc-prod
+docker push kleinchang/efcsydney-roster:$BUILD_TAG
+dockerImg="docker.io/kleinchang/efcsydney-roster:${BUILD_TAG:-latest}"
+file='./ecs_service.json'
+stack='ecs-service'
+
+aws cloudformation deploy \
+    --capabilities CAPABILITY_IAM \
+    --template-file $file \
+    --stack-name $stack \
+    --parameter-overrides \
+        Environment=$env \
+        Certificates=$cert \
+        DockerImgSrc=$dockerImg \
+        NodeEnv=$NodeEnv \
+        S3ConfBucketName=$bucket
