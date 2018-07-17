a1a4b233c477ff441548f3fd42f0162e9f24a2c9
LiamLai <liam.lai@tabcorp.com.au>
2018-07-17 22:36:59 +1000

    travis-deploy docker service

---
+++ docker_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# create parameter
+if [ $1 = "qa" ]; then
+    env='QA'
+    NodeEnv='qa'
+    bucket='efc-sydney-qa'
+else
+    env='Production'
+    NodeEnv='production'
+    bucket='efc-sydney-prod'
+
+fi
+
+docker-compose -f docker/docker-compose.yml build efc-prod
+docker push kleinchang/efcsydney-roster:${BUILD_TAG:-latest}
+dockerImg="docker.io/kleinchang/efcsydney-roster:${BUILD_TAG:-latest}"
+file='aws/ecs_service.json'
+stack='ecs-service'
+
+aws --region ap-southeast-2 cloudformation deploy \
+    --capabilities CAPABILITY_IAM \
+    --template-file $file \
+    --stack-name $stack \
+    --parameter-overrides \
+        Environment=$env \
+        Certificates=$cert \
+        DockerImgSrc=$dockerImg \
+        NodeEnv=$NodeEnv \
+        S3ConfBucketName=$bucket
