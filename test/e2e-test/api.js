62382613852c38e26f41e396c961f930d1ff3029
josephj <josephj6802@gmail.com>
2018-07-30 08:33:56 +1000

    #341 Ensure data always resets

--- test/e2e-test/api.js
+++ test/e2e-test/api.js
@@ -5,20 +5,6 @@ const apiUrl = isDev
   ? 'http://localhost:3001'
   : 'https://demo-roster.efcsydney.org';
 
-export function createServiceInfo({ category, date }) {
-  return fetch(`${apiUrl}/api/serviceInfo`, {
-    method: 'POST',
-    body: JSON.stringify({
-      category,
-      date,
-      footnote: '',
-      skipService: false,
-      skipReason: ''
-    }),
-    headers: { 'Content-Type': 'application/json' }
-  }).then(res => res.json());
-}
-
 export function getEvents({ category, fromDate, toDate }) {
   return fetch(
     `${apiUrl}/api/events?category=${category}&from=${fromDate}&to=${toDate}`
@@ -36,5 +22,5 @@ export function modifyServiceInfo(id, { category, date }) {
       skipReason: ''
     }),
     headers: { 'Content-Type': 'application/json' }
-  });
+  }).then(res => res.json());
 }
