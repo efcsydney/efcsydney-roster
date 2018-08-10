8acabc78af7b0137ec965128a0f809ede7703213
KyleH <yhua045@gmail.com>
2018-08-10 22:29:30 +1000

    #363: Added validation handling

--- api/service/user-service.js
+++ api/service/user-service.js
@@ -3,12 +3,32 @@ const {
   updateUser,
   getUsers: getAllUsers
 } = require('../data/user-repository');
+const {
+  validate: validateUser
+} = require('../models-validator/user-validator');
+const ValidationError = require('../models-validator/validation-error');
+const ValidationResult = require('../models-validator/validation-result');
+const Sequelize = require('sequelize');
 
 async function saveUser(user) {
-  if (user.id) {
-    return await updateUser(user);
-  } else {
-    return await createUser(user);
+  const validationResult = validateUser(user);
+  if (!validationResult.success) {
+    throw new ValidationError(validationResult);
+  }
+
+  try {
+    if (user.id) {
+      return await updateUser(user);
+    } else {
+      return await createUser(user);
+    }
+  } catch (err) {
+    if (err instanceof Sequelize.UniqueConstraintError) {
+      throw new ValidationError(
+        new ValidationResult('The email already exists in the database')
+      );
+    }
+    throw err;
   }
 }
 
