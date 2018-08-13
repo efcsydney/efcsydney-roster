4a24dc6ccd484d6f6ddcbbfe4de8deb7608137e7
LiamLai <liam.lai@tabcorp.com.au>
2018-08-14 00:55:18 +1000

    fix mysqldump

--- docker/Entrypoint.sh
+++ docker/Entrypoint.sh
@@ -5,7 +5,24 @@ cd $EFC_FOLDER
 
 prod() {
   export PATH=$PATH:~/.local/bin/
-  aws ssm get-parameters --region ap-southeast-2 --names username password database host dialect --with-decryption --query 'Parameters[*].{key:Name,value:Value}' | jq from_entries | jq '{ "'$NODE_ENV'" : .}' > ./config/database.json
+  aws ssm get-parameters --region ap-southeast-2 \
+    --names username password database host dialect \
+    --with-decryption \
+    --query 'Parameters[*].{key:Name,value:Value}' \
+    | jq from_entries \
+    > parameter.json
+
+  cat parameter.json | jq '{ "'$NODE_ENV'" : .}' > ./config/database.json
+
+  user=`jq -r .username parameter.json`
+  password=`jq -r .password parameter.json`
+
+  cat > ~/.my.cnf <<EOF
+[mysqldump]
+user=$user
+password=$password
+EOF
+
   yarn db-migrate
 
   aws s3 cp s3://$S3_BUCKET/email-list.csv $EFC_FOLDER/db/data/email-list.csv
