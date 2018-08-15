558fc442f5a5941b98978e88c05d876fc8c6bdd5
josephj <josephj6802@gmail.com>
2018-08-15 21:42:24 +1000

    Add script to update database.json

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
 web: npm run server
+release: node ./heroku/update-database-config.js
+release: npm run db-migrate
+
---
+++ heroku/update-database-config.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const _ = require('lodash');
+const { promisify } = require('util');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
+
+async function exec() {
+  console.log('Updating the config/database.json ...'); // eslint-disable-line
+  const app = await heroku.get('/apps/efcsydney-roster/config-vars');
+  const url = _.get(app, 'CLEARDB_DATABASE_URL', '');
+  const matches = url.match(/mysql:\/\/([^:]+):([^@]+)@([^/]+)\/(.+)$/);
+  const config = {
+    production: _.defaults(
+      {
+        username: matches[1],
+        password: matches[2],
+        host: matches[3]
+      },
+      {
+        username: 'root',
+        password: null,
+        host: '127.0.0.1',
+        database: 'efcRoster',
+        dialect: 'mysql',
+        logging: false
+      }
+    )
+  };
+
+  await promisify(fs.writeFile)(
+    __dirname + '/../config/database.json',
+    JSON.stringify(config, null, 2)
+  );
+
+  console.log('Updated successfully'); // eslint-disable-line
+}
+
+exec().catch(e => {
+  console.log(`Failed - ${e.body.message}`); // eslint-disable-line
+});
