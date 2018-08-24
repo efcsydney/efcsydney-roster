0f4cef3bc3d656ca2520c295a0bc085d74f972bd
Choco Li <cli@modernstar.com>
2018-08-24 12:04:19 +1000

    remove code

--- test/e2e-test/api.js
+++ test/e2e-test/api.js
@@ -5,20 +5,6 @@ const apiUrl = isDev
   ? 'http://localhost:3001'
   : 'https://demo-roster.efcsydney.org';
 
-export function createServiceInfo({ category, date }) {
-  return fetch(`${apiUrl}/api/serviceInfo`, {
-    method: 'POST',
-    body: JSON.stringify({
-      category,
-      date,
-      footnote: '',
-      skipService: false,
-      skipReason: ''
-    }),
-    headers: { 'Content-Type': 'application/json' }
-  }).then(res => res.json());
-}
-
 export function getEvents({ category, fromDate, toDate }) {
   return fetch(
     `${apiUrl}/api/events?category=${category}&from=${fromDate}&to=${toDate}`
