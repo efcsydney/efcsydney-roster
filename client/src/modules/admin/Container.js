d0fd0d95e52a85e8a0f8064f6f624e6e5c054cd2
Choco <trust2065@example.com>
2018-09-08 11:45:06 +1000

    resolve conflict

--- client/src/modules/admin/Container.js
+++ client/src/modules/admin/Container.js
@@ -6,7 +6,8 @@ import Nav from './services/Nav';
 import { Auth, NavBar } from 'modules/core';
 import AdminServices from './services';
 import AdminEmail from './email';
-import UnderConstruction from './UnderConstruction';
+import AdminChangelogs from './changelogs';
+import AdminUsers from './users';
 import styled from 'styled-components';
 import { media } from 'styled';
 
@@ -72,16 +73,8 @@ export default class Admin extends PureComponent {
               <Switch>
                 <Route path="/admin/services" component={AdminServices} />
                 <Route path="/admin/email" component={AdminEmail} />
-                <Route
-                  path="/admin/changelogs"
-                  render={() => (
-                    <UnderConstruction title="Changelogs Management" />
-                  )}
-                />
-                <Route
-                  path="/admin/users"
-                  render={() => <UnderConstruction title="Users Management" />}
-                />
+                <Route path="/admin/changelogs" component={AdminChangelogs} />
+                <Route path="/admin/users" component={AdminUsers} />
                 <Redirect to="/admin/services" />
               </Switch>
             </Content>
