cc48d559ca71dc0b923acaa6d3e042f1ce1eaebd
josephj <josephj6802@gmail.com>
2018-11-07 07:00:32 +1100

    #473 Upgrade to react-script 2.1 with TypeScript native supported

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,25 +1,15 @@
 const rewireStyledComponents = require('react-app-rewire-styled-components');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const {
-  rewireWebpack: rewireTypescript,
-  rewireJest: rewireTypescriptJest,
-  rewireTSLint
-} = require('react-app-rewire-typescript-babel-preset');
 
 module.exports = {
   webpack: function(config, env) {
-    config = rewireTypescript(config);
     config = rewireStyledComponents(config, env, {
       ssr: true,
       fileName: false,
       displayName: true
     });
     config = rewireReactHotLoader(config, env);
-    config = rewireTSLint(config);
 
     return config;
-  },
-  jest: function(config) {
-    return rewireTypescriptJest(config);
   }
 };
